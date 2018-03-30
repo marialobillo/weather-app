7ea7fab0d7d418208dadc9ae863dcad2b97c6d1d
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-03-30 20:42:48 +0200

    adding first components

---
+++ src/components/WeatherLocation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Location from './Location';
+import WeatherData from './WeatherData';
+
+const WeatherLocation = () => (
+	<div>Weather Location
+		<Location />
+		<WeatherData />
+	</div>
+);
+
+export default WeatherLocation;
