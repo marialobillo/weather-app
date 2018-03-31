f9dbb11a7bc848bd49ff679b3258983e88a9788d
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-03-31 11:40:15 +0200

    adding weathertemperature and weatherextrainfo components

--- src/components/WeatherLocation.js
+++ src/components/WeatherLocation.js
@@ -3,7 +3,7 @@ import Location from './Location';
 import WeatherData from './WeatherData';
 
 const WeatherLocation = () => (
-	<div>Weather Location
+	<div>
 		<Location />
 		<WeatherData />
 	</div>
