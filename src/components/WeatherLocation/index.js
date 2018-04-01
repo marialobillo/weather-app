0adaa985460bee78708334e3a59bd07ae35a7f6f
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-04-01 18:44:38 +0200

    remodeling and fixing css

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
+import {CLOUD, CLOUDY, SUN, RAIN, SNOW, WINDY} from './../../constants/weathers';
 import './style.css';
+
+const data = {
+	temperature: 18,
+	weatherState: SUN,
+	humidity: 65,
+	wind: '10 m/s'
+};
 
 const WeatherLocation = () => (
 	<div className="WeatherLocationCont">
 		<Location city={'New Your, US'}/>
-		<WeatherData />
+		<WeatherData data={data}/>
 	</div>
 );
 
